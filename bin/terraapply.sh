f6fe4bca88786b3c3c39ed92c346db939f54cd64
Justin <justin.campbell@hpe.com>
2017-02-18 20:46:32 +0000

    updated with env variables

--- bin/terraapply.sh
+++ bin/terraapply.sh
@@ -2,6 +2,10 @@
 
 # BASH script that creates the Terraform Plan 
 
+TERRAGLOBAL=/home/ubuntu/terraform/global/base_aws.tfvars
+
+export TERRAGLOBAL
+
 # Perform Apply 
 
 terraform apply -var-file=$TERRAGLOBAL
--- bin/terraplan.sh
+++ bin/terraplan.sh
@@ -2,4 +2,8 @@
 
 # BASH script that creates the Terraform Plan 
 
+TERRAGLOBAL=/home/ubuntu/terraform/global/base_aws.tfvars
+
+export TERRAGLOBAL
+
 terraform plan -var-file=$TERRAGLOBAL -out=$1.out
