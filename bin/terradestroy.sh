1df9c3a4667162e5dc5c9dad7fb329320dd68cd3
Justin <justin.campbell@hpe.com>
2017-02-18 21:32:50 +0000

    update with vars

--- bin/terradestroy.sh
+++ bin/terradestroy.sh
@@ -2,4 +2,8 @@
 
 # BASH script that creates the Terraform Plan 
 
+TERRAGLOBAL=/home/ubuntu/terraform/global/base_aws.tfvars
+
+export TERRAGLOBAL
+
 terraform destroy -force -var-file=$TERRAGLOBAL
